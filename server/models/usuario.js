55e19e667468f4375736c2ccfe997ca5044514d8
israel <israel.bejarano.a@gmail.com>
2019-03-10 23:19:55 +0100

    add forma mas eficiente de ocultar psw en el JSON

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -41,6 +41,13 @@ let usuarioSchema = new Schema({
     }
 });
 
+// para ocultar el password en el JSON de vuelta de las peticiones
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+};
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
