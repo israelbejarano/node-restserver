cf1cb8be1f8cd41e2fe07ddf031acdf54c399302
israel <israel.bejarano.a@gmail.com>
2019-03-12 23:06:16 +0100

    get usuarios con paginacion y total

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,7 +6,29 @@ const _ = require('underscore');
 const Usuario = require('../models/usuario');
 
 app.get('/usuario', function(req, res) {
-    res.json('get usuario');
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+        .limit(limite)
+        .skip(desde)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count({}, (err, total) => {
+                res.status(200).json({
+                    ok: true,
+                    usuarios,
+                    total
+                });
+            });
+        });
 })
 
 app.post('/usuario', function(req, res) {
