2d1f113a2f4ca246edb2bc83014b9491dcc98e03
israel <israel.bejarano.a@gmail.com>
2019-03-15 23:15:39 +0100

    create SEED process env y CADUCIDAD_TOKEN

--- server/config/config.js
+++ server/config/config.js
@@ -18,3 +18,13 @@ if (process.env.NODE_ENV === 'dev') {
     urlBD = process.env.MONGO_URI;
 }
 process.env.URLBD = urlBD;
+
+// ========================================
+// vencimiento del token
+// ========================================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ========================================
+// seed de autenticacion
+// ========================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
