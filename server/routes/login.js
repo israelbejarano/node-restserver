a81ac5ac99bc065eb490d46a4b3036e2ced33a41
israel <israel.bejarano.a@gmail.com>
2019-03-15 23:14:03 +0100

    add login post metodo

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Usuario = require('../models/usuario');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.status(200).json({
-        ok: true
+    let body = req.body;
+    Usuario.findOne({ email: body.email }, (err, usuarioBD) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioBD) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario incorrecto.'
+                }
+            });
+        }
+        if (!bcrypt.compareSync(body.password, usuarioBD.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario incorrecto.'
+                }
+            });
+        }
+        let token = jwt.sign({ usuario: usuarioBD }, 'este-es-el-seed-desarrollo', { expiresIn: process.env.CADUCIDAD_TOKEN });
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBD,
+            token: token
+        });
     });
 });
 
