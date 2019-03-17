935d2d5ab058d9324a87bca3b0064539bf37c34b
israel <israel.bejarano.a@gmail.com>
2019-03-17 12:28:36 +0100

    habilitar la carpeta public

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 const colors = require('colors');
 const express = require('express');
 const mongoose = require('mongoose'); // moongose
+const path = require('path');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,6 +11,9 @@ app.use(bodyParser.json()); // parse application/json
 
 // configuracion global de rutas
 app.use(require('./routes/index'));
+
+// habilitar la carpeta public para poder hacer login con google
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Conexion a BBDD
 mongoose.connect(process.env.URLBD, { useNewUrlParser: true, useCreateIndex: true }, // cadena de conexion para mongo resuelve deprecateds
