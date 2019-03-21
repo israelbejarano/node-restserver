03fa61b81c34c9df53bc66245beda41767a7453a
israel <israel.bejarano.a@gmail.com>
2019-03-21 23:24:14 +0100

    obtener imagen

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 let app = express();
 
+app.get('/imagen/:tipo/:img', (req, res) => {
+    let tipo = req.params.tipo;
+    let img = req.params.img;
 
+    let pathImg = `./uploads/${tipo}/${img}`;
+    let pathNoImage = path.resolve(__dirname, '../assets/no-image.jpg');
+    res.sendFile(pathNoImage);
+});
 
 module.exports = app;
