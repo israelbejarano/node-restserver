8401ee556b44ef6c29eb23fa3703371e7fb76dde
israel <israel.bejarano.a@gmail.com>
2019-03-21 23:09:12 +0100

    metodo para subir imagenes para usuario y producto

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const path = require('path');
 const app = express();
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
 
 app.use(fileUpload({ useTempFiles: true })); // default options
 
-app.post('/upload', function(req, res) {
+app.put('/upload/:tipo/:id', function(req, res) {
+    let tipo = req.params.tipo;
+    let id = req.params.id;
     if (!req.files) {
         return res.status(400).json({
             ok: false,
@@ -13,6 +19,17 @@ app.post('/upload', function(req, res) {
             }
         });
     }
+
+    //validar tipo
+    let tiposValidos = ['productos', 'usuarios'];
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            message: 'Los tipos permitidos son ' + tiposValidos.join(', '),
+            tipo
+        });
+    }
+
     let archivo = req.files.archivo;
     let nombreCortado = archivo.name.split('.');
     let extension = nombreCortado[nombreCortado.length - 1];
@@ -25,18 +42,95 @@ app.post('/upload', function(req, res) {
         });
     }
 
-    archivo.mv(`uploads/${archivo.name}`, (err) => {
-        if (err)
+    // cambiar el nombre del archivo
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
+
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
+        if (err) {
             return res.status(500).json({
                 ok: false,
                 err
             });
-
-        res.json({
-            ok: true,
-            message: 'imagen subida correctamente'
-        });
+        }
+        if (tipo === 'usuarios') {
+            imagenUsuario(id, res, nombreArchivo);
+        } else {
+            imagenProducto(id, res, nombreArchivo);
+        }
     });
 });
+
+function imagenUsuario(id, res, nombreArchivo) {
+    Usuario.findById(id, (err, usuarioBD) => {
+        if (err) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioBD) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'no existe el usuario en BBDD'
+                }
+            });
+        }
+
+        borrarArchivo(usuarioBD.img, 'usuarios');
+
+        usuarioBD.img = nombreArchivo;
+        usuarioBD.save((err, usuarioGuardado) => {
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            });
+        });
+
+    });
+}
+
+function imagenProducto(id, res, nombreArchivo) {
+    Producto.findById(id, (err, productoBD) => {
+        if (err) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!productoBD) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'no existe el producto en BBDD'
+                }
+            });
+        }
+
+        borrarArchivo(productoBD.img, 'productos');
+
+        productoBD.img = nombreArchivo;
+        productoBD.save((err, productoGuardado) => {
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            });
+        });
+
+    });
+}
+
+function borrarArchivo(nombreImagen, tipo) {
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+    if (fs.existsSync(pathImagen)) {
+        fs.unlinkSync(pathImagen);
+    }
+}
 
 module.exports = app;
