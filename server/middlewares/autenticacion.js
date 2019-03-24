f9fb3fc553803352e5ee434278c128468d8db4a1
israel <israel.bejarano.a@gmail.com>
2019-03-24 11:40:18 +0100

    verificarTokenImg por paramentro de url

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -34,4 +34,21 @@ let verificaAdminRole = (req, res, next) => {
     }
 };
 
-module.exports = { verificaToken, verificaAdminRole };
+// ========================================
+// verificar la imagen por token url
+// ========================================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+module.exports = { verificaToken, verificaAdminRole, verificaTokenImg };
